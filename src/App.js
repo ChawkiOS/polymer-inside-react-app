3b5757faed55259c66e45c7ce3dd023a2e041e96
ChawkiOS <chawkios2@gmail.com>
2020-02-02 02:02:42 +0100

    show my polymer element inside the react app

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <my-main-element prop="Hello, I am a polymer element!"></my-main-element>
         <a
           className="App-link"
           href="https://reactjs.org"
